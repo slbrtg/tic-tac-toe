64ccd3ee43057b7b76326c269d786191a4d9cbf0
Saul Artiga <slbrtg@gmail.com>
2017-06-14 13:59:38 -0700

    create game functions for players to exist and make moves

--- js/scripts.js
+++ js/scripts.js
@@ -5,28 +5,53 @@ function Game (){
   this.row2.length = 3
   this.row3 = [];
   this.row3.length = 3
+  this.numOfPlayers = 1;
 }
 
-function Player (symbol){
-  this.symbol = symbol;
-  this.winner = false;
-  this.input = input;
+//creates players or bots based on input
+Game.prototype.setPlayers = function(userNum){
+  this.numOfPlayers = userNum;
+  if(this.numOfPlayers === 1) {
+    playerx = new Player("X");
+  } else {
+    playerx = new Player("X");
+    playero = new Player("O");
+  }
+  console.log(playero + " " + playerx);
 }
 
-Game.prototype.receiveInput = function (input, playerSymbol) {
+//accepts a players input and marks the board according to
+//position and player symbol
+Game.prototype.receiveInput = function (input, symbol) {
   if (input < 3){
-    this.row1[input]= playerSymbol;
+    this.row1[input]= symbol;
   } else if (input > 5){
     input -= 6;
-    this.row3[input]= playerSymbol;
+    this.row3[input]= symbol;
   } else {
     input -= 3;
-    this.row2[input]= playerSymbol;
+    this.row2[input]= symbol;
   }
   console.log(newGame.row1);
   console.log(newGame.row2);
   console.log(newGame.row3);
 }
+
+//construct for Player object
+function Player (symbol){
+  this.symbol = symbol;
+  this.winner = false;
+  this.userInput = 0;
+}
+
+
+
+//allows player to put a mark on the board
+Player.prototype.play = function(game,input,symbol) {
+  console.log(symbol +" makes a move");
+  game.receiveInput(input,symbol);
+}
+
 
 
 
